6df7ac282dbd04e8d0ca0a613a69590499247270
ManuelVelasco99 <manuelvelascodevs@gmail.com>
2021-07-29 00:12:21 -0300

    Se modifico la ruta de productos del admin. Ahora los productos se muestran con su ultimo precio y su categoria. Tambien al insertar un producto se inserta su precio inicial.

--- Backend/controllers/adminProductos.ts
+++ Backend/controllers/adminProductos.ts
@@ -2,10 +2,22 @@ import { Request,Response } from "express";
 import query from "../bin/mysqlConection";
 import IproductCreate from "../interfaces/productCreate";
 
+async function getProducts(){
+    await query(`drop temporary table if exists product_price`);
+    await query(`create temporary table product_price
+    SELECT PRODUCT.id_product,MAX(PRICE.fecha_desde) fecha_desde  
+    FROM PRODUCT INNER JOIN PRICE ON PRODUCT.id_product=PRICE.id_product GROUP BY PRODUCT.id_product`);
+    const products = await query(`SELECT PRODUCT.*,PRICE.fecha_desde,PRICE.price, category.nombre nombre_categoria  FROM PRODUCT INNER JOIN PRICE ON PRODUCT.id_product=PRICE.id_product  
+	INNER JOIN product_price ON product.id_product=product_price.id_product AND product_price.fecha_desde=price.fecha_desde 
+    INNER JOIN category on product.id_category = category.id_category`);
+    return products;
+}
+
 export={
 
     showProducts:async(req:Request, res:Response) =>{
-        try{const products = await query('SELECT * FROM PRODUCT');
+        try{
+        const products = await getProducts();
         res.json({estado:'success',products:products})
         }
         catch(error){
@@ -28,10 +40,8 @@ export={
             id_product:insertProduct.insertId,
             price:precioProducto
         };
-        const insertPrecio = await query('INSERT INTO PRICE SET ?',[precio]);
-        console.log(insertPrecio);
-        await query("commit")
-        
+        await query('INSERT INTO PRICE SET ?',[precio]);
+        await query("commit");      
         res.json({
             estado:'success',product:insertProduct,
         })}
